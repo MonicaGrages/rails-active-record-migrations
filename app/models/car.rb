b04fb519fd3d6182a4d3c03f712300d9ef79fb03
MonicaGrages <mgrages1@gmail.com>
2017-06-20 19:29:52 -0400

    completed rails active record migrations and validations questions

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,7 @@
 class Car < ApplicationRecord
+  validates :make, presence: true
+  validates :model, presence: true
+  validates :year, presence: true, length: {is: 4}
+  validates :sunroof, inclusion: {in: [true, false]}
+  validates :color, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620222731) do
+ActiveRecord::Schema.define(version: 20170620230513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20170620222731) do
     t.string "make"
     t.string "model"
     t.integer "year"
-    t.boolean "moonroof"
+    t.boolean "sunroof"
     t.string "color"
   end
 
